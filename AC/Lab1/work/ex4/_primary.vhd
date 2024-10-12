2fc54f9d5144c6d386b121e0055e060c93237ee5
Alex Tunea <alextuneaa@gmail.com>
2024-10-12 20:37:54 +0300

    idk

--- AC/Lab1/work/ex4/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity ex4 is
-    port(
-        i               : in     vl_logic_vector(5 downto 0);
-        is6             : out    vl_logic
-    );
-end ex4;
--- AC/Lab1/work/ex4_tb/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity ex4_tb is
-end ex4_tb;
